ecf283b9022ead0c9a1a9e5da59a3ad09f7ec4de
leonbyrdjr <leonbyrdjr@Leons-MacBook-Pro.local>
2023-02-22 17:31:39 -0600

    commit tree b2b033b45cead84d3c40b4b5d35137ba909b9661

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Your code here
+
+
+function createEmployeeRecord(employeeRecord){
+const employeeObj = {firstName: employeeRecord[0],
+familyName: employeeRecord[1],
+title: employeeRecord[2],
+payPerHour: employeeRecord[3],
+timeInEvents: [],
+timeOutEvents: []}
+    return employeeObj
+}
+
+function createEmployeeRecords(employeeRecords){
+const employeeArrObj = employeeRecords.map(employeeRecord => createEmployeeRecord(employeeRecord))
+return employeeArrObj
+}
+
+function createTimeInEvent(employeeRecord, dateStamp){
+    const hour = dateStamp.split(" ")[1]
+    const date = dateStamp.split(" ")[0]
+    const timeInEvent = {type: 'TimeIn', hour: parseInt(hour), date: date}
+employeeRecord.timeInEvents.push(timeInEvent)
+return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord, dateStamp){
+    const hour = dateStamp.split(" ")[1]
+    const date = dateStamp.split(" ")[0]
+    const timeOutEvent = {type: 'TimeOut', hour: parseInt(hour), date: date}
+employeeRecord.timeOutEvents.push(timeOutEvent)
+return employeeRecord
+}
+
+function hoursWorkedOnDate(employeeRecord){
+const timeIn = employeeRecord.timeInEvents[0].hour
+const timeOut = employeeRecord.timeOutEvents[0].hour
+const hoursWorked = parseInt((timeOut - timeIn) / 100)
+return hoursWorked
+}
+
+function wagesEarnedOnDate(employeeRecord){
+const timeIn = employeeRecord.timeInEvents[0].hour
+const timeOut = employeeRecord.timeOutEvents[0].hour
+const hoursWorked = parseInt((timeOut - timeIn) / 100)
+const wages = hoursWorked * employeeRecord.payPerHour
+return wages
+}
+
+function allWagesFor(employeeRecord){
+const timeIn1 = employeeRecord.timeInEvents[0].hour
+const timeIn2 = employeeRecord.timeInEvents[1].hour
+const timeOut1 = employeeRecord.timeOutEvents[0].hour
+const timeOut2 = employeeRecord.timeOutEvents[1].hour
+const day1 = (timeOut1 - timeIn1)/100
+const day2 = (timeOut2 - timeIn2)/100
+const allHours = (day1 + day2)
+const allWages = allHours * employeeRecord.payPerHour
+return allWages
+}
+
+function calculatePayroll(employeeRecord){
+console.log(employeeRecord)
+}
+
+
+
